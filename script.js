eb43e33a56acf4506f49a1f8b8db7864546f167a
Chun Khai <chunkhai.k@gmail.com>
2023-06-20 09:44:02 +0800

    Add CLI custom-tab-tile

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ const readline = require('readline');
 async function monitorElementStatus() {
   const websiteUrl = 'https://ticketmaster.sg/activity/detail/24_coldplay';
   const elementSelector = '#hlLinkToQueueTicket2';
-  const delayBetweenTabs = 6000; // 10 seconds
-  const monitoringInterval = 10000; // 5 seconds
+  const delayBetweenTabs = 5000; // 10 seconds
+  const monitoringInterval = 5000; // 5 seconds
 
   const browser = await puppeteer.launch({ headless: false });
   const mainPage = await browser.newPage();
-  const openPages = [mainPage]; // Store references to all open pages
+  const openTabs = [{ page: mainPage, title: 'Main Tab' }]; // Store references to all open tabs with custom titles
 
-  let previousTexts = Array(openPages.length).fill(''); // Array to store previous element texts
+  let previousTexts = Array(openTabs.length).fill(''); // Array to store previous element texts
 
-  // Define a helper function to evaluate the status and text of the element on a specific page
-  const evaluateElementStatus = async (page, tabIndex) => {
+  // Define a helper function to evaluate the status and text of the element on a specific tab
+  const evaluateElementStatus = async (tab, tabIndex) => {
     try {
+      const page = tab.page;
       await page.waitForSelector(elementSelector);
       const element = await page.$(elementSelector);
       const elementText = await page.evaluate((el) => el.innerText, element);
@@ -37,24 +38,25 @@ async function monitorElementStatus() {
   // Monitor the element status at a regular interval
   setInterval(async () => {
     const elementStatuses = await Promise.all(
-      openPages.map((page, index) => evaluateElementStatus(page, index))
+      openTabs.map((tab, index) => evaluateElementStatus(tab, index))
     );
 
     // Create and display the table
     const table = new Table({
-      head: ['Status', 'Element Text'],
-      colWidths: [20, 40],
+      head: ['Tab Title', 'Status', 'Element Text'],
+      colWidths: [20, 20, 40],
     });
 
     elementStatuses.forEach((status, index) => {
-      const row = [status.status, status.text];
+      const { title } = openTabs[index];
+      const row = [title, status.status, status.text];
       table.push(row);
     });
 
     console.log(table.toString());
   }, monitoringInterval);
 
-  // Open additional websites in new tabs
+  // Open additional websites in new tabs with custom titles
   const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -68,10 +70,31 @@ async function monitorElementStatus() {
 
   while (true) {
     const newPage = await browser.newPage();
-    openPages.push(newPage); // Add the new page reference to the openPages array
+    const customTitle = `Tab ${openTabs.length + 1}`;
+    openTabs.push({ page: newPage, title: customTitle }); // Add the new tab reference with custom title to the openTabs array
     await newPage.goto(websiteUrl);
     await newPage.waitForTimeout(delayBetweenTabs);
+    await newPage.evaluate((customTitle) => {
+      document.title = customTitle; // Set the browser tab title to the custom title
+    }, customTitle);
   }
+
+  // Function to handle user input for focusing on a specific tab
+  const handleUserInput = async () => {
+    input.question('Enter the row index to bring the corresponding tab to the front: ', (index) => {
+      const tabIndex = parseInt(index, 10);
+      if (tabIndex >= 0 && tabIndex < openTabs.length) {
+        const tab = openTabs[tabIndex];
+        const { page } = tab;
+        page.bringToFront();
+      } else {
+        console.log('Invalid row index. Please enter a valid row index.');
+      }
+      handleUserInput();
+    });
+  };
+
+  handleUserInput();
 }
 
 monitorElementStatus();
