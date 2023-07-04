57c57c11d5ba58cb18374d8da5cb351f229d3327
Chun Khai <chunkhai.k@gmail.com>
2023-07-04 17:33:20 +0800

    Code refactoring

--- scripts/script-cache.js
+++ scripts/script-cache.js
@@ -10,26 +10,21 @@ async function monitorElementStatus() {
 
   // const finalURL = `${scraperAPI}?api_key=${API_KEY}&url=${targetURL}`;
   const finalURL = 'https://ticketmaster.sg/activity/detail/24_taylorswift';
-  
-  // Create and display the table
-  const table = new Table({
-    head: ['Tab Title', 'Status', 'Element Text'],
-    colWidths: [20, 20, 40],
-  });
 
-  // const elementSelector = '#hlLinkToQueueTicket2';
   const elementSelector = '.an-bk';
   const delayBetweenTabs = 5000; // 5 seconds
-  const monitoringInterval = 5000; // 5 seconds
 
-  const browser = await puppeteer.launch({ headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const mainPage = await browser.newPage();
-  const openTabs = [{ page: mainPage, title: 'Main Tab' }]; // Store references to all open tabs with custom titles
+  const openTabs = [{ page: mainPage, title: 'Main Tab' }];
 
-  let previousTexts = Array(openTabs.length).fill(''); // Array to store previous element texts
+  let previousTexts = Array(openTabs.length).fill('');
 
-  // Define a helper function to evaluate the status and text of the element on a specific tab
-  const evaluateElementStatus = async (tab, tabIndex) => {
+  const table = createTable();
+
+  await mainPage.goto(finalURL);
+
+  async function evaluateElementStatus(tab, tabIndex) {
     try {
       const page = tab.page;
       await page.waitForSelector(elementSelector);
@@ -38,40 +33,34 @@ async function monitorElementStatus() {
 
       const status = elementText === previousTexts[tabIndex] ? 'Unchanged' : 'Changed';
 
-      previousTexts[tabIndex] = elementText; // Update the previous element text
+      previousTexts[tabIndex] = elementText;
 
       return { status, text: elementText };
     } catch (error) {
       return { status: 'Error', text: 'Error evaluating element status' };
     }
-  };
+  }
 
-  // Open initial website in the main tab
-  await mainPage.goto(finalURL);
-
-  // Monitor the element status at a regular interval
-  async function monitorElementStatus() {
-    const elementStatuses = await Promise.all(
-      openTabs.map((tab, index) => evaluateElementStatus(tab, index))
-    );
-      
-    // clears table then re-enters every single element
-    table.splice(0);
-
-    elementStatuses.forEach((status, index) => {
-      if(status.status === 'Changed') {        
-        const { title } = openTabs[index];
-        const row = [title, status.status, status.text];
-        table.push(row);
-      }
+  function createTable() {
+    return new Table({
+      head: ['Tab Title', 'Status', 'Element Text'],
+      colWidths: [20, 20, 40],
     });
   }
 
-  // Open additional websites in new tabs with custom titles
+  function clearTable() {
+    table.splice(0);
+  }
+
+  function addToTable(title, status, text) {
+    const row = [title, status, text];
+    table.push(row);
+  }
+
   const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-  });  
+  });
 
   input.question('Press any key and Enter to stop the script...\n', async () => {
     input.close();
@@ -82,13 +71,27 @@ async function monitorElementStatus() {
   while (true) {
     const newPage = await browser.newPage();
     const customTitle = `Tab ${openTabs.length + 1}`;
-    openTabs.push({ page: newPage, title: customTitle }); // Add the new tab reference with custom title to the openTabs array
+    openTabs.push({ page: newPage, title: customTitle });
+
     await newPage.goto(finalURL);
     await newPage.waitForTimeout(delayBetweenTabs);
     await newPage.evaluate((customTitle) => {
-      document.title = customTitle; // Set the browser tab title to the custom title
+      document.title = customTitle;
     }, customTitle);
-    monitorElementStatus();
+
+    const elementStatuses = await Promise.all(
+      openTabs.map((tab, index) => evaluateElementStatus(tab, index))
+    );
+
+    clearTable();
+
+    elementStatuses.forEach((status, index) => {
+      if (status.status === 'Changed') {
+        const { title } = openTabs[index];
+        addToTable(title, status.status, status.text);
+      }
+    });
+
     console.log(table.toString());
   }
 }
