b6e4cd30d57112396b9e690777264f4441a842e4
Chun Khai <chunkhai.k@gmail.com>
2023-07-07 10:27:00 +0800

    Add commands

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ require('dotenv').config();
 // API/Keys Declaration
 const BROWSER_ENDPOINT = process.argv[2]
 
-const taytayURL = 'https://google.com'
+const EUNICE_EMAIL = process.env.EUNICE_EMAIL
+const EUNICE_PASSSWORD = process.env.EUNICE_PASSWORD
+const CK_EMAIL1 = process.env.CHUNKHAI_EMAIL1
+const CK_EMAIL2 = process.env.CHUNKHAI_EMAIL2
+const CK_PASSWORD = process.env.CHUNKHAI_PASSWORD
+
+const EMAIL_ADDRESS = EUNICE_EMAIL
+const PASSWORD = EUNICE_PASSSWORD
+
+const taytayURL = 'https://www.google.com/search?q=taylorswift&oq=taylorswift&aqs=chrome..69i57.1830j0j1&sourceid=chrome&ie=UTF-8'
 const initialURL = 'https://ticketmaster.sg/login';
 const finalURL = 'https://click.mailing.ticketmaster.com/?qs=2d32134119df64f25e745e413bfa5c0d7adb93969de6de7c11f82ff431185145ea34971a579f8e42bd8a57691121379a8f9a5576413829b491030b11f2a019f5';
 
@@ -47,39 +56,64 @@ var isPaused = false;
   // main method 1 : opens a new tab + keeps reference to it via openTabs
   async function openTab() {
     try {
-      var tabsOpened = openTabs.length + 1;
-      const newPage = await browser.newPage();
-      const customTitle = `Tab ${tabsOpened}`;
-      openTabs.push({ page: newPage, title: customTitle });
+      if (openTabs.length <= 60) {
+        var tabsOpened = openTabs.length;
+        const newPage = await browser.newPage();
+        const customTitle = `Tab ${tabsOpened}`;
+        openTabs.push({ page: newPage, title: customTitle });
+    
+        await newPage.goto(initialURL);
+    
+        const signInFormUsernameExists = await newPage.$('#signInFormUsername');
+        const signInFormPasswordExists = await newPage.$('#signInFormPassword');
+    
+        if (signInFormUsernameExists && signInFormPasswordExists) {
+          await newPage.waitForSelector('#signInFormUsername');
+          await newPage.type('#signInFormUsername', EMAIL_ADDRESS);
+    
+          await newPage.waitForSelector('#signInFormPassword');
+          await newPage.type('#signInFormPassword', PASSWORD);
+        }
+    
+        await newPage.waitForSelector('.btn-primary');
+        await newPage.click('.btn-primary');
+        
+        await new Promise(resolve => setTimeout(resolve, 3000))
   
-      await newPage.goto(initialURL);
-  
-      const signInFormUsernameExists = await newPage.$('#signInFormUsername');
-      const signInFormPasswordExists = await newPage.$('#signInFormPassword');
-  
-      if (signInFormUsernameExists && signInFormPasswordExists) {
-        await newPage.waitForSelector('#signInFormUsername');
-        await newPage.type('#signInFormUsername', 'ooijessie@hotmail.com');
-  
-        await newPage.waitForSelector('#signInFormPassword');
-        await newPage.type('#signInFormPassword', 'Battleground.1');
+        await newPage.goto(finalURL);
+        
+        await newPage.evaluate((customTitle) => {
+          document.title = customTitle;
+        }, customTitle);
+    
+        console.log("Number of tabs opened: " + tabsOpened);
       }
-  
-      await newPage.waitForSelector('.btn-primary');
-      await newPage.click('.btn-primary');
-      
-      await newPage.goto(finalURL);
-      
-      await newPage.evaluate((customTitle) => {
-        document.title = customTitle;
-      }, customTitle);
-  
-      console.log("Number of tabs opened: " + tabsOpened);
     } catch (error) {
       // Handle the error
       console.error("An error occurred while opening the tab:", error);
     }
   }  
+
+  async function navigateAllTabs(url) {
+    const refreshTabs = openTabs.map(tab => {
+      return new Promise(async (resolve) => {
+        await tab.page.goto(url)
+      })
+    })
+
+    await Promise.all(refreshTabs)
+  }
+
+  async function refreshAllTabs() {
+    console.log('test2')
+    const refreshTabs = openTabs.map(tab => {
+      return new Promise(async (resolve) => {
+        await tab.page.reload()
+      })
+    })
+
+    await Promise.all(refreshTabs)
+  }
 
   const input = readline.createInterface({
     input: process.stdin,
@@ -91,12 +125,23 @@ var isPaused = false;
       isPaused = !isPaused;
       if (isPaused) {
         clearInterval(OPEN_TAB_ID);
-        clearInterval(EVAL_ELEM_ID);
         console.log('Script paused. Press Enter again to resume.');
+      
       } else {
-        console.log('Script resumed.');
-        init();
+          console.log('Script resumed.');
+          init();
+        }  
+    } else if (isPaused) {
+      if (input.trim() === 'goto') {
+        navigateAllTabs(finalURL)
+        console.log('All tabs redirected to new URL')
+      
+      } else if (input.trim() === 'refresh') {
+        refreshAllTabs();
+        console.log('All tabs refreshed')
       }
+    } else {
+      console.log('failed')
     }
   });
 
